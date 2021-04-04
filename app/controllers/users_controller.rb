00bfcc50ec3464d082d323aad1b32a5d374813e3
ChristianCudby <christiancudby@hotmail.com>
2021-04-03 23:12:59 -0400

    finishes the user controller code

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,3 +16,12 @@ class UsersController < ApplicationController
             render :new
         end
     end
+
+
+    private
+
+    def user_params
+        params.require(:user).permit(:username, :password, :password_confirmation)
+    end
+
+end
