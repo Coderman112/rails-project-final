5e5ecfb5807849ad2f566eafb3fd9371db427541
ChristianCudby <christiancudby@hotmail.com>
2021-04-04 05:03:30 -0400

    adds routes for ratings and for the signup and login actions

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,14 @@ Rails.application.routes.draw do
   resources(:games, except: [:put, :edit, :patch, :destroy, :update]) do
     resources :measurements, only: [:new, :create]
   end
+
+  resources :ratings, only: [:new, :create, :index, :update]
+
+  get '/signup', to: "users#new", as: "signup"
+  post '/signup', to: "users#create"
+  get '/login', to: "sessions#new", as: "login"
+  post '/login', to: "sessions#create"
+  post '/logout', to: "sessions#destroy"
+  get "/auth/facebook/callback", to: "sessions#create_with_fb"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
