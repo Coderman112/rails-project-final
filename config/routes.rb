12f6bacd61bc0618712faa726b4d00061bf83dc6
ChristianCudby <christiancudby@hotmail.com>
2021-04-04 04:57:16 -0400

    adds routes for users and games

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  resources :users, except: [:show, :index, :new, :create, :destroy, :patch, :put] do
+    resources(:games, except: [:show, :edit, :update, :destroy, :put])
+  end
+
+  resources(:games, except: [:put, :edit, :patch, :destroy, :update]) do
+    resources :measurements, only: [:new, :create]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
